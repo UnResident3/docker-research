c695e6d76e469a56edbc16c2916e02056980a381
Tanoo Kacha <tanoo.kacha@g.swu.ac.th>
2019-11-26 13:22:17 +0700

    THESIS-156  Edit  funneioz  sugges

--- thesis-156/run-thesis-156.sh
+++ thesis-156/run-thesis-156.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
 
-docker network create -d bridge --subnet=172.20.0.0/16 --gateway=172.20.0.1 thesis-156
+docker network create -d bridge \
+--subnet=172.20.0.0/16 \
+--gateway=172.20.0.1 \
+thesis-156
 
-docker build --file THESIS-155/Dockerfile -t thesis-155:latest .
+docker build -t \
+thesis-155:latest \
+./thesis-155/
+# ถ้าในกรณีมี Dockerfile หลายไฟล์หรือไม่ได้ตั้งชื่อ Dockerfile ควรใช้วิธีนี้
+# docker build \
+# -t thesis-155:latest \
+# --file ../thesis-155/Dockerfile .
 
-docker run -d --network=thesis-156 --ip=172.20.0.5 --name=db-server thesis-155:latest
+docker run --rm -d \
+--network=thesis-156 \
+--ip=172.20.0.5 \
+--name=db-server \
+thesis-155:latest
 
-docker run -d --network=thesis-156 --ip=172.20.0.10 --name=db-admin --link postgres:db -p 10081:8080 adminer:4.7.5-standalone
+docker run --rm -d \
+--network=thesis-156 \
+--ip=172.20.0.10 \
+--name=db-admin \
+--link postgres:db \
+-p 10081:8080 \
+adminer:4.7.5-standalone
